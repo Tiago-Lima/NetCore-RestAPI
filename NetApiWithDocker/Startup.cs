52c7860a47fbe889ca15a7a61f72e1d04c5f1f22
Tiago-Lima <tiago.santoslima91@gmail.com>
2023-05-10 12:29:33 -0300

    Configuando suporte a Migrations na classe Startup, utilizando as dependências Evolve e Serilog

--- NetApiWithDocker/Startup.cs
+++ NetApiWithDocker/Startup.cs
@@ -9,38 +9,54 @@ using Microsoft.Extensions.Logging;
 using NetApiWithDocker.Business.Implementations;
 using NetApiWithDocker.Model.Context;
 using NetApiWithDocker.Repository.Implementations;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace NetApiWithDocker
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+        
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+         
             services.AddControllers();
-            services.AddApiVersioning();
+            
 
             var connection = Configuration["MySQLConnection:MySQLConnectionString"]; // string de conexão
 
             services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
 
+            if (Environment.IsDevelopment())
+            {
+                MigrateDatabase(connection);
+            }
+
+
+            services.AddApiVersioning();
             //Injeção de dependências
 
             services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
             services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
         }
+
+     
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -58,6 +74,26 @@ namespace NetApiWithDocker
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private void MigrateDatabase(string connection)
+        {
+            try
+            {
+                var evolveConnection = new MySql.Data.MySqlClient.MySqlConnection(connection);
+                var evolve = new EvolveDb.Evolve(evolveConnection, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true,
+                };
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database migration failed", ex);
+                throw;
+            }
+            
         }
     }
 }
