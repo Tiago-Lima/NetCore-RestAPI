9210d7ed943ed9d04f720da53f52b6a601340a58
Tiago-Lima <tiago.santoslima91@gmail.com>
2023-06-12 17:24:47 -0300

    Iniciando implementação do padrão Generic Repository

---
+++ NetApiWithDocker/Repository/Generic/GenericRepository.cs
@@ -0,0 +1,42 @@
+﻿using NetApiWithDocker.Model.Base;
+using NetApiWithDocker.Repository.Implementations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetApiWithDocker.Repository.Generic
+{
+    public class GenericRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        public T Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(long Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> FindAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T FindById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Update(T item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ NetApiWithDocker/Repository/Generic/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using NetApiWithDocker.Model;
+using NetApiWithDocker.Model.Base;
+using System.Collections.Generic;
+
+namespace NetApiWithDocker.Repository.Implementations
+{
+    public interface IRepository<T> where T : BaseEntity // Interface genérica do tipo T onde obrigatóriamente precisa extender a interface BaseEntity
+    {
+        T Create(T item);
+        T FindById(long id);
+        List<T> FindAll();
+        T Update(T item);
+        void Delete(long Id);
+        bool Exists(long id);
+    }
+}
