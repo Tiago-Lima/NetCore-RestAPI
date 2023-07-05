0575df24103061016ef3c8aa70917f00dcd6686f
Tiago-Lima <tiago.santoslima91@gmail.com>
2023-07-05 17:02:06 -0300

    Iniciando implementação HATEOAS

---
+++ NetApiWithDocker/Hypermedia/Abstract/ISuportHyperMedia.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetApiWithDocker.Hypermedia.Abstract
+{
+    interface ISuportHyperMedia
+    {
+    }
+}
